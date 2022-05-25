54a3988ca4c8944436ff56fc1a8a27399e2a5b2c
unknown <tsrogers8@msn.com>
2022-05-25 08:55:09 -0600

    Created Product Serialiazer Class and File

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.forms import CharField
 # Create your models here.
 
 
-class Products(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=300)
     description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=8, decimal_places=2)
